6cf1d50f4fbf557371300247057d88fcf5c501d1
James Young <youn5393@vandals.uidaho.edu>
2019-03-05 07:25:54 +0000

    Added goblin fortune telling ---Changelog--- Added goblin fortune telling. At the moment, this functions exactly the same as a magic 8-ball, but this can be expanded into its own class should it seem fun enough.

--- goblin_bot_2.0.py
+++ goblin_bot_2.0.py
@@ -41,7 +41,7 @@ async def on_message(message):
     print("input text:", text)
     msg = ''
     if text[0] == '!G':
-        if text[1] == 'give' or 'take' or 'uwu':
+        if text[1] == 'give' or 'take' or 'uwu' or 'fortune':
             msg = Misc_Goblin.create_output(text)
         if text[1] == 'help':
             msg = Help_Goblin.create_output(text)
