c03df608e4ff2fdcff8379414dc2797990accf14
James Young <youn5393@vandals.uidaho.edu>
2019-04-09 03:51:09 +0000

    Init_bot fully implemented, bug fixes * Added the initiative module, can now roll, add, list, and delete   entities * Fixed an issue with trying to read from .data files if they didn't   exist

--- init_bot.py
+++ init_bot.py
@@ -4,7 +4,14 @@ class init_goblin:
     def __init__(self):
         print("init_goblin created")
         self.entities = []
-        self.fp = open("player_init.data", "r+")
+        try:
+            self.fp = open("player_init.data", "r+")
+        except IOError:
+            self.fp = open("player_init.data", "w")
+            self.fp.close()
+            self.fp = open("player_init.data", "r+")
+        
+        #self.fp = open("player_init.data", "r+")
         for line in self.fp:
             #name init
             words = line.split()
@@ -12,16 +19,16 @@ class init_goblin:
             entity['name'] = words[0]
             entity['init'] = int(words[1])
             self.entities.append(entity)
-            print("Added:" + entity['name']
-                           + str(entity['init']))
+            print("Added:" + entity['name']+"("
+                           + str(entity['init']) + ")")
 
     def __del__(self):
         self.fp.seek(0)
         self.fp.truncate(0)
         for entity in self.entities:
             hold_str = ""
-            hold_str += entity['entity']
-            hold_str += str(entity['init'])
+            hold_str += entity['name'] + " "
+            hold_str += str(entity['init']) + "\n"
             self.fp.write(hold_str)
         self.fp.close()
         
@@ -31,6 +38,8 @@ class init_goblin:
         #!G init roll [name] (bonus)
         if text[2] == 'roll':
             msg = self.roll_init(text)
+        if text[2] == 'add':
+            msg = self.add_init(text)
         if text[2] == 'list':
             msg = self.list_init(text)
         if text[2] == 'clear':
@@ -45,6 +54,12 @@ class init_goblin:
         entity = {}
         
         entity['name'] = name
+        #if name is in list, return error message
+        for e in self.entities:
+            if entity['name'] == e['name']:
+                msg = '```Entity already exists```'
+                return msg
+        
         msg += '```'
         msg += name
         msg += '\'s initiative is '
@@ -53,13 +68,20 @@ class init_goblin:
             roll = random.randrange(1, 21)
             msg += str(roll)
             if text[4][0] == '-':
-                roll -= int(text[4][1])
+                temp = text[4][1:]
+                roll -= int(temp)
                 msg += text[4]
-            if text[4][0] == '+':
-                roll += int(text[4][1])
+            elif text[4][0] == '+':
+                temp = text[4][1:]
+                roll += int(temp)
+                msg += text[4]
+            elif text[4].isdigit():
+                #assumes the bonus is positive
+                roll += int(text[4])
+                msg += '+'
                 msg += text[4]
             msg += " : "
-            msg += roll
+            msg += str(roll)
             entity['init'] = roll 
         #straight roll
         else:
@@ -68,10 +90,32 @@ class init_goblin:
             entity['init'] = roll 
         
         self.entities.append(entity)
+        self.entities.sort(key=lambda r: r['init'], reverse = True)
+        
+        msg += '```'
+        return msg
+        
+    def add_init(self, text):
+        msg = ''
+        #!G init add Dazzak 3
+        entity = {}
+        entity['name'] = text[3]
+        entity['init'] = int(text[4])
+
+        self.entities.append(entity)
+        self.entities.sort(key=lambda r: r['init'], reverse = True)
+
+        msg += '```'
+        msg += text[3]
+        msg += ' added to table with initiative '
+        msg += text[4]
+        msg += '```'
+        
         return msg
         
     def list_init(self, text):
         msg = ''
+        msg += '**__Initiative Order__**'
         msg += '```'
         
         if not self.entities:
