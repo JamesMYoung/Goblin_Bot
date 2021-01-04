9c0332c96dd77beed26b5cbeae6041901525a695
JamesMYoung <youn5393@vandals.uidaho.edu>
2021-01-03 23:33:35 -0700

    Adds specific number of rerolls

--- yaccdice.py
+++ yaccdice.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.5
 import ply.yacc as yacc
 import random
+import copy
 
 from lexdice import tokens
 
@@ -174,9 +175,7 @@ def p_roll(p):
     'roll : NUMBER D NUMBER'
     global msg
     
-    #print("dice-roll: ", end='')
     msg += "dice-roll: "
-    #print(str(p[1]) + "d" + str(p[3]))
     msg += str(p[1]) + "d" + str(p[3]) + "\n"
     
     rolls = []
@@ -277,6 +276,128 @@ def p_roll_reroll(p):
             rolls[count] = roll
         count += 1
         
+    print("rolls after:")
+    print(rolls)
+    
+    if rerolled:
+        roll_sum = 0
+        if p[1] > 1:
+            msg += ' new rolls: '
+        else:
+            msg += ' new roll: '
+        
+        for roll in rolls:
+            # If roll hits max, do something special
+            if roll == p[3]:
+                msg += '*' + str(roll) + '*'
+            else:
+                msg += str(roll) 
+            msg += ', '
+            roll_sum += roll
+        #lazily trims off last ', '
+        msg = msg[:-2]
+        msg += '\n'
+    
+    msg += ' total: '
+    msg += str(roll_sum)
+    if roll_sum <= 1:
+        msg += ' *oof*'
+    msg += '\n'
+        
+    print(rolls)
+    print(roll_sum)
+    
+    #p[0] = p[1] + p[3]
+    p[0] = roll_sum
+    
+def p_roll_reroll_lim(p):
+    '''roll : NUMBER D NUMBER RR NUMBER COLON LPAREN reroll_list RPAREN'''
+    global msg
+    print(msg)
+    # Clear message from term_list output
+    #msg = '```'
+    
+    for pp in p:
+        print(pp)
+    msg += "dice-roll: "
+    msg += str(p[1]) + "d" + str(p[3]) + ", rerolling: ("
+    
+    rr_list = []
+    
+    if isinstance(p[8], list):
+        rr_list = p[8]
+        for i in range(len(p[8])-1):
+            msg += str(p[8][i]) + ", "
+        msg += str(p[8][-1])
+    else:
+        rr_list.append(p[8])
+        msg += str(p[8])
+        
+        
+    msg += ")  " + str(p[5]) + " time(s)\n"
+    
+    print(rr_list)
+    
+    rolls = []
+    for i in range(0, p[1]):
+        roll = random.randrange(1, p[3]+1)
+        rolls.append(roll)
+    
+    roll_sum = 0
+    if p[1] > 1:
+        msg += ' rolls: '
+    else:
+        msg += ' roll: '
+       
+    for roll in rolls:
+        # If roll hits max, do something special
+        if roll == p[3]:
+            msg += '*' + str(roll) + '*'
+        else:
+            msg += str(roll) 
+        msg += ', '
+        roll_sum += roll
+    #lazily trims off last ', '
+    msg = msg[:-2]
+    msg += '\n'
+    
+    print("rolls before:")
+    print(rolls)
+    
+    
+    # sort list ascending
+    # save reroll value
+    # reroll
+    # search for previous value
+    # first instance found, replace
+    
+    # a more costly refinement is needed to make
+    # output "perfect"
+    
+    
+    sort_rolls = copy.copy(rolls)
+    sort_rolls.sort()
+    print(sort_rolls)
+    
+    count = 0
+    rerolled = False
+    reroll_count = p[5]
+    
+    for roll in sort_rolls:
+        if roll in rr_list and reroll_count !=0:
+            rerolled = True
+            msg += " reroll!: " + str(roll) + " -> "
+            old_roll = roll
+            roll = random.randrange(1, p[3]+1)
+            msg += str(roll) + "\n"
+            
+            for c in range(len(rolls)):
+                if rolls[c] == old_roll:
+                    rolls[c] = roll
+                    break
+            reroll_count -= 1
+            print(str(reroll_count) + " rerolls left")
+    
     print("rolls after:")
     print(rolls)
     
